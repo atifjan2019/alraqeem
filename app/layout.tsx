1d039b46b906a1a39937f136d6dc6b286d05653b
khyberwear <atifjan2019@gmail.com>
2026-07-14 13:36:21 +0100

    feat: adjust viewport settings to prevent zooming on mobile devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: "#0A211A",
 };
 
